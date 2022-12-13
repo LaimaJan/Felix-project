be2ed2747cf9c5f039e0dcf7832cd42cc2ba8f5c
LaimaJan <lm.lingyte@gmail.com>
2022-12-13 21:10:07 +0200

    created modal logic for movie trailers

--- src/app/pages/SingleMovie/SingleMovie.js
+++ src/app/pages/SingleMovie/SingleMovie.js
@@ -7,7 +7,6 @@ import Footer from '../../components/Footer';
 import { withRouter } from '../../components/useParam/UseParams';
 import SingleMovieCard from '../../components/SingleMovieCard/SingleMovieCard';
 
-// import MovieCard from '../../components/MovieCard';
 // import { FaRegPlayCircle } from 'react-icons/fa';
 
 import './SingleMovie.css';
@@ -24,6 +23,7 @@ class SingleMovie extends React.Component {
 			loading: false,
 			error: false,
 			favorites: retrieveID,
+			openModal: false,
 		};
 
 		console.log('favorites this.state');
@@ -47,7 +47,11 @@ class SingleMovie extends React.Component {
 		}
 	}
 
-	clickToWatchTrailer() {}
+	WatchTrailer() {
+		console.log('clicked');
+		const currentState = this.state.openModal;
+		this.setState({ openModal: !currentState });
+	}
 
 	async componentDidMount() {
 		this.setState({ loading: true });
@@ -56,7 +60,6 @@ class SingleMovie extends React.Component {
 		try {
 			const tokenNumber = localStorage.getItem('token');
 			// console.log('TOKENAS SingleMovie: ' + tokenNumber);
-
 			const result = await fetch(
 				`https://dummy-video-api.onrender.com/content/items/${id}`,
 				{
@@ -66,14 +69,12 @@ class SingleMovie extends React.Component {
 					},
 				}
 			);
-
 			if (result.status >= 400 && result.status <= 599) {
 				this.setState({ error: true });
 			} else {
 				let response = await result.json();
 				// console.log('RESPONSAS');
-				// console.log(response);
-
+				console.log(response);
 				this.setState({ singleMovie: response });
 			}
 		} catch (error) {
@@ -86,27 +87,41 @@ class SingleMovie extends React.Component {
 	render() {
 		const { loading, error, singleMovie, favorites } = this.state;
 		return (
-			<div className="singleMovie-wrapper">
-				<Header>
-					<Button>
-						<Link to="/">Logout</Link>
-					</Button>
-				</Header>
-				<main>
-					{loading && <img src={logo} className="App-logo" alt="logo" />}
-					{error && <p>Whoops! Failed to Load! 🙊</p>}
-					<SingleMovieCard
-						id={singleMovie.id}
-						key={singleMovie.id}
-						title={singleMovie.title}
-						description={singleMovie.description}
-						image={singleMovie.image}
-						onHandleClick={() => this.handleClick(singleMovie.id)}
-						isFavorite={favorites.includes(singleMovie.id)}
-						clickWatchTrailer={this.clickToWatchTrailer()}
+			<div className="content-wrapper">
+				<div className="singleMovie-wrapper">
+					<Header>
+						<Button>
+							<Link to="/">Logout</Link>
+						</Button>
+					</Header>
+					<main>
+						{loading && <img src={logo} className="App-logo" alt="logo" />}
+						{error && <p>Whoops! Failed to Load! 🙊</p>}
+						<SingleMovieCard
+							id={singleMovie.id}
+							key={singleMovie.id}
+							title={singleMovie.title}
+							description={singleMovie.description}
+							image={singleMovie.image}
+							onHandleClick={() => this.handleClick(singleMovie.id)}
+							isFavorite={favorites.includes(singleMovie.id)}
+							clickWatchTrailer={this.WatchTrailer.bind(this)}
+						/>
+					</main>
+					<Footer />
+				</div>
+				<div
+					className={
+						this.state.openModal ? 'show-video-modal' : 'disable-video-modal'
+					}
+				>
+					<iframe
+						title="movieTrailer"
+						src={singleMovie.video}
+						frameBorder="0"
+						allowFullScreen
 					/>
-				</main>
-				<Footer />
+				</div>
 			</div>
 		);
 	}
